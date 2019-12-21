4624d6c73c721d7693a0fb9e3686f7611b6fa63d
Adam Coster <adam@bscotch.net>
2019-12-21 13:52:29 -0600

    feature(Dev): Add linting and typescript configuration files.

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  "env": {
+      "es6": true,
+      "node": true,
+      "mocha": true,
+      "browser":true
+  },
+  "extends": [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended"
+  ],
+  "globals": {
+      "Atomics": "readonly",
+      "SharedArrayBuffer": "readonly"
+  },
+  parser: "@typescript-eslint/parser",
+  "plugins": [
+      "@typescript-eslint"
+  ],
+  "parserOptions": {
+      "ecmaVersion": 6,
+      "sourceType": "module"
+  },
+  "rules": {
+      "@typescript-eslint/indent": [2,2],
+      "@typescript-eslint/ban-ts-ignore": "off",
+      "@typescript-eslint/type-annotation-spacing": "off",
+      "@typescript-eslint/member-delimiter-style":"off",
+      "@typescript-eslint/explicit-function-return-type":"off",
+      "@typescript-eslint/no-explicit-any":"off",
+      "@typescript-eslint/no-empty-function":"off",
+      "no-empty":"off",
+      'semi':'error',
+      "no-trailing-spaces": "error",
+      "getter-return":"off"
+  },
+  "overrides": [
+      {
+        "files": ["*.js"],
+        "rules": {
+          "@typescript-eslint/no-var-requires": "off"
+        }
+      }
+  ]
+};
---
+++ src/test/index.ts
@@ -0,0 +1,5 @@
+import {expect} from "chai";
+
+describe("Rumpus CE Client", async function(){
+  xit("can do something", async function(){});
+});
